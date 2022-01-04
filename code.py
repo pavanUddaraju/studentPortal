4cf1a09922f4ba51b17724332a15879d41d0c224
pavanUddaraju <97045359+pavanUddaraju@users.noreply.github.com>
2022-01-04 10:21:59 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,21 @@
+class Student:
+  def __init__(self, data):
+    self.regNum = data[0]
+    self.name = data[1]
+    self.branch = data[2]
+    self.phoneNum = data[3]
+  
+  def getData(data):
+    return (data.regNum, data.name, data.branch, data.phoneNum)
+
+def writeDataTofile(studentData):
+  inputData = open("input.txt", "r+").readlines()
+  for entry in range(len(inputData)):
+    listOfStudent = inputData[entry].split(" ")
+    regNum = int(listOfStudent[0])
+    studentObject = studentData[regNum]
+    inputData[entry] = studentObject.getData()
+
 # This method handles user request and provides the required information
 def handleUserRequest(n, studentsData):
     if n != 1 and n != 2:
@@ -6,29 +24,26 @@ def handleUserRequest(n, studentsData):
     index = -1
     print("Enter student register number")
     regNum = int(input())
-    for entry in range(len(studentsData)):
-        if studentsData[entry][1] == regNum:
-            index = entry
-    if index == -1:
+    if regNum not in studentsData:
         print("Register number not found")
         return
     if n == 1:
-        print(studentsData[index])
+        print(Student.getData(studentsData[regNum]))
     else:
         print("select the field you want to change\n1. Name\n2. phone Number\n3. Branch")
         selectedValue = int(input())
         if selectedValue == 1:
             print("Enter name")
             name = input()
-            studentsData[index][0] = name
+            studentsData[regNum].name = name
         elif selectedValue == 2:
             print("Enter Phone Number to update")
             phoneNum = input()
-            studentsData[index][3] = phoneNum
+            studentsData[regNum].phoneNum = phoneNum
         elif selectedValue == 3:
             print("Enter branch to update")
             branch = input()
-            studentsData[index][2] = branch
+            studentsData[regNum].branch = branch
         else:
             print("Invalid request")
             return
@@ -36,7 +51,12 @@ def handleUserRequest(n, studentsData):
 # main method holds input dataset        
 def main():
     print("Welcome to Student Portal\nSelect your request")
-    studentsData = [["name1", 171, "ECE", "94947868"], ["name2", 172, "EEE", "6281655"]]
+    inputData = open("input.txt", "r")
+    studentsData = dict()
+    for entry in inputData:
+      eachStudent = entry.split(" ")
+      regNum = int(eachStudent[0])
+      studentsData[regNum] = Student(eachStudent)
     while(1):
         print("Enter 1 to display data\nEnter 2 to update data")
         n = int(input())
@@ -46,6 +66,6 @@ def main():
         if flowPoint == 'n':
             print("Thanks for using student Portal")
             break
-            
+    writeDataTofile(studentsData)
 #method call to trigger the flow            
 main()
